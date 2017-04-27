5afac3617f5d1a854e55f8ec9825a17d2b44af99
kiranpaturi <kiran_p@k3informatics.com>
2017-04-27 15:33:45 +0530

    added code

--- app/products/products.js
+++ app/products/products.js
@@ -15,16 +15,60 @@
                 });
             return dfd.promise;
         };
+
+        function datamapper(res) {
+            var response = {
+                data: {
+                    products: []
+                }
+            }
+            angular.forEach(res.data.items, function(item, index) {
+                var product = {
+                    index: index,
+                    Model: item.name,
+                    Price: item.salePrice,
+                    Description: item.shortDescription
+                }
+                response.data.products.push(product);
+            });
+            return response;
+        };
+        this.getProductsFromWM = function(search) {
+            //var url = "WM/&query=" + search;
+            var url = "WM/" + search;
+            var dfd = $q.defer();
+            $http.get(url)
+                .then(function(response) {
+                    var result = datamapper(response);
+                    dfd.resolve(result);
+                })
+                .catch(function(errorResponse) {
+                    dfd.reject(errorResponse);
+                });
+            return dfd.promise;
+        };
     }
 
     function productCtrl(productSvc) {
         var vm = this;
-        productSvc.getProducts()
-            .then(function(response) {
-                vm.products = response.data.products;
-            })
-            .catch(function(errorResponse) {
-                console.log(errorResponse);
-            });
+        vm.sort = {
+            model: "Model"
+        };
+        vm.orderByModel = function() {
+            vm.sort.model = vm.sort.model == "Model" ? "-Model" : "Model";
+        };
+
+        vm.searchProducts = function() {
+            productSvc.getProductsFromWM(vm.search)
+                .then(function(response) {
+                    vm.products = response.data.products;
+                    console.log(response);
+                })
+                .catch(function(response) {
+                    console.log(response);
+                });
+
+        };
+
     }
 })();
